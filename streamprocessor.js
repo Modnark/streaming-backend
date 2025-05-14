9c4c88f6fe0443344fb723ff566c8a50c46d72a9
Modnark <modnarkdev@tuta.com>
2025-05-14 15:43:20 -0400

    logging

--- streamprocessor.js
+++ streamprocessor.js
@@ -4,7 +4,7 @@ const config = require('./config.json');
 
 function createNewStream(streamKey) {
     const ffmpeg = spawn('ffmpeg', [
-        '-i', `rtmp://localhost/live/${streamKey}`,
+        '-i', `rtmp://localhost/golive/${streamKey}`,
         '-c:v', 'libx264', '-preset', 'veryfast', '-g', '30', '-sc_threshold', '0',
         '-c:a', 'aac', '-ar', '44100', '-b:a', '128k',
         '-f', 'hls',
@@ -16,6 +16,18 @@ function createNewStream(streamKey) {
         '-hls_segment_filename', `${config.server.streamStorage}/${streamKey}/dat_%d.m4s`,
         `${config.server.streamStorage}/${streamKey}/dat.m3u8`        
     ]);
+
+    ffmpeg.stdout.setEncoding('utf8');
+    ffmpeg.stdout.on('data', (data) => {
+        console.log('STDOUT');
+        console.log(data);
+    });
+
+    ffmpeg.stderr.setEncoding('utf8');
+    ffmpeg.stderr.on('data', (data) => {
+        console.log('STDERR');
+        console.log(data);
+    });
 
     IStreams.set(streamKey, ffmpeg);
     console.log(`Stream active for ${streamKey}`);
