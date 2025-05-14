c1bfe76581b1d43dde960d3ee2b722a2f01b880d
Modnark <modnarkdev@tuta.com>
2025-05-14 16:52:04 -0400

    deletion works, renamed init.mp4, changed some ffmpeg options to improve speed

--- streamprocessor.js
+++ streamprocessor.js
@@ -7,14 +7,14 @@ const path = require('path');
 function createNewStream(streamKey) {
     const ffmpeg = spawn('ffmpeg', [
         '-i', `rtmp://localhost/golive/${streamKey}`,
-        '-c:v', 'libx264', '-preset', 'veryfast', '-g', '30', '-sc_threshold', '0',
+        '-c:v', 'libx264', '-preset', 'ultrafast', '-g', '30', '-sc_threshold', '0',
         '-c:a', 'aac', '-ar', '44100', '-b:a', '128k',
         '-f', 'hls',
         '-hls_time', '1', // time in seconds of each segment
-        '-hls_list_size', '4', // max size of segments, older get removed
+        '-hls_list_size', '10', // max size of segments, older get removed
         '-hls_flags', 'delete_segments+append_list+independent_segments+split_by_time',
         '-hls_segment_type', 'fmp4',
-        '-hls_fmp4_init_filename', 'init.mp4',
+        '-hls_fmp4_init_filename', `${config.server.streamStorage}/${streamKey}_init.mp4`,
         '-hls_segment_filename', `${config.server.streamStorage}/${streamKey}_dat_%d.m4s`,
         `${config.server.streamStorage}/${streamKey}_dat.m3u8`        
     ]);
