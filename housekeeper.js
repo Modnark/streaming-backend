c84976b1c411a512e1fc8fd35006370e74dde993
Modnark <modnarkdev@tuta.com>
2025-05-15 21:02:22 -0400

    fix housekeeper

--- housekeeper.js
+++ housekeeper.js
@@ -1,26 +1,30 @@
 const fs = require('fs/promises');
 const path = require('path');
 const config = require('./config.json');
+const staleThreshold = config.server.houseKeeperStaleThreshold * 1000;
 
 // Ticks every minute, deletes stale m3u8 or m4s files
 async function houseKeeper() {
     const filePath = config.server.streamStorage;
     try {
         const files = await fs.readdir(filePath); // get all files in the storage folder
+        const filteredFiles = files.filter(file => {
+            const ext = path.extname(file).toLowerCase();
+            return ['.m3u8', '.mp4', '.m4s'].includes(ext);
+        });
 
-        for(const file of files) {
-            const mTime = (await fs.stat(path.join(filePath, file))).mtimeMs; // get modified time of each file
-            const delta = Date.now() - mTime;
-            if(delta >= 60000) { // Check if files are older than 1 minute
-                const ext = path.extname(file);
-                if(ext === '.m3u8' || ext === '.m4s' || ext === '.mp4') {
-                    console.log(`Removing stale file ${file} with delta of ${delta}`);
-                    await fs.unlink(path.join(filePath, file)); // Delete stale files
-                }
-            }
+        for(const file of filteredFiles) {
+             // get modified time of each file
+            const ext = path.extname(file);
+            const stats = await fs.stat(path.join(filePath, file));
+            const fTime = ext === '.mp4' ? stats.atimeMs : stats.mtimeMs;
+            const delta = Date.now() - fTime;
+
+            if(delta >= staleThreshold) // Check if files are older than 1 minute
+                await fs.unlink(path.join(filePath, file)); // Delete stale files
         }
     } catch(error) {
-        console.log(error); // TODO: handle these in a better way
+        console.log(`[!][HK] ${error}`); // TODO: handle these in a better way
     }
 }
 
